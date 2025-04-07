bdb1ff149dbec1f61952e83efc3c40fa7f6c44bd
ArnauOliveras <77742762+ArnauOliveras@users.noreply.github.com>
2025-04-07 15:14:09 +0200

    rapatu

--- Assets/Scripts/Characters/Rapatu/RapatuCharacterController.cs
+++ Assets/Scripts/Characters/Rapatu/RapatuCharacterController.cs
@@ -27,6 +27,11 @@ public class RapatuCharacterController : CharacterMaster
     public float m_HealingAreaRadius = 500;
     public float m_PercentageSkillPower = 50;
     public float m_PercentageAdditionalLife = 7;
+    RapatuEHealingArea m_HealingAreaE;
+    bool m_CanStopE = false;
+    public float m_AmimDelayE = 0.5f;
+    public float m_ECurrentHealingTime = 0;
+    public float m_EMaxHealingTime = 0;
 
     [Header("R SKILL")]
     public float m_RangeR = 1000;
@@ -51,6 +56,8 @@ public class RapatuCharacterController : CharacterMaster
         }
         if (m_ESkill.GetUsingSkill() && m_CanStopE)
         {
+            m_ECurrentHealingTime += Time.deltaTime;
+            GetCharacterUI().UpdateCastingUI(m_ECurrentHealingTime, m_EMaxHealingTime);
             if (Input.GetKeyDown(m_ESkillKey))
             {
                 StartCoroutine(StopESkill());
@@ -88,6 +95,7 @@ public class RapatuCharacterController : CharacterMaster
         if (m_RSkill.GetUsingSkill())
             return;
 
+        StopAttacking();
         base.ESkill();
         m_ESkill.SetUsingSkill(true);
         SetAnimatorTrigger("IsUsingE");
@@ -97,6 +105,10 @@ public class RapatuCharacterController : CharacterMaster
             StopMovement();
         StartCoroutine(ESkillCoroutine());
 
+        GetCharacterUI().SetCastingUIAbilityText("Curando");
+        GetCharacterUI().HideCastingTime();
+        GetCharacterUI().UpdateCastingUI(0, 1);
+
     }
     IEnumerator StopESkill()
     {
@@ -104,22 +116,23 @@ public class RapatuCharacterController : CharacterMaster
         if (!GetIsLookingForPosition())
             StopMovement();
         SetDisabled(false);
+        GetCharacterUI().HideCastingUI();
         StopCoroutine(ESkillCoroutine());
         Destroy(m_HealingAreaE.gameObject);
         yield return new WaitForSeconds(0.1f);
         m_CanStopE = false;
         m_ESkill.SetUsingSkill(false);
     }
-    RapatuEHealingArea m_HealingAreaE;
-    bool m_CanStopE = false;
-    public float m_AmimDelayE = 0.5f;
+
     IEnumerator ESkillCoroutine()
     {
         yield return new WaitForSeconds(m_AmimDelayE);
         m_HealingAreaE = Instantiate(m_HealingAreaEPrefab, transform.position, Quaternion.identity).GetComponent<RapatuEHealingArea>();
         m_HealingAreaE.SetHealingAria(transform, m_HealingAreaRadius / 100);
+        m_ECurrentHealingTime = 0;
+        m_EMaxHealingTime = m_HealingTimes - 1 * m_HealingTimeSpace;
         m_CanStopE = true;
-
+        GetCharacterUI().ShowCastingUI();
 
         float l_HealLifeValue = (m_ESkill.GetAttribute("Curación", GetESkillLevel())) + (m_PercentageSkillPower / 100) * GetCharacterStats().GetAbilityPower() + (m_PercentageAdditionalLife / 100) * GetCharacterStats().GetBonusHealth();
 
@@ -143,12 +156,17 @@ public class RapatuCharacterController : CharacterMaster
         if (m_ESkill.GetUsingSkill())
             return;
 
+        StopAttacking();
         m_RSkill.SetUsingSkill(true);
         SetDisabled(true);
         SetAnimatorTrigger("IsUsingR");
         if (!GetIsLookingForPosition())
             StopMovement();
         m_TimerR = 0;
+        GetCharacterUI().SetCastingUIAbilityText("Cargando salto");
+        GetCharacterUI().HideCastingTime();
+        GetCharacterUI().UpdateCastingUI(0, 1);
+        GetCharacterUI().ShowCastingUI();
     }
     float m_TimerR;
     public float m_MinTimeToBeRedy = 1f;
@@ -157,30 +175,19 @@ public class RapatuCharacterController : CharacterMaster
     {
         if (!m_Jumping)
         {
-            if (Input.GetKey(KeyCode.R))
+            m_TimerR += Time.deltaTime;
+            Debug.LogError("Cargando...");
+            if (m_TimerR >= m_AutoJumpSecondsR)
             {
-                m_TimerR += Time.deltaTime;
-                Debug.LogError("Cargando...");
-                if (m_TimerR >= m_AutoJumpSecondsR)
-                {
-                    StartJumpingR();
-                }
-            }
-            else if (Input.GetKeyUp(KeyCode.R) && m_TimerR >= m_MinTimeToBeRedy)
-            {
-                Debug.LogError("R Up");
                 StartJumpingR();
             }
-            else
-            {
-                Debug.LogError("R Jump because you Up R");
-                StartJumpingR();
-            }
+            GetCharacterUI().UpdateCastingUI(m_TimerR, m_AutoJumpSecondsR);
         }
     }
     void StartJumpingR()
     {
         base.RSkill();
+        GetCharacterUI().HideCastingUI();
         m_Jumping = true;
         SetAnimatorTrigger("RJump");
         JumpR();
