46259aee6fff2d465d31ce0f04c1fab3b08c6e93
ArnauOliveras <77742762+ArnauOliveras@users.noreply.github.com>
2025-04-07 20:12:49 +0200

    rapatu skills

--- Assets/Scripts/Characters/Rapatu/RapatuCharacterController.cs
+++ Assets/Scripts/Characters/Rapatu/RapatuCharacterController.cs
@@ -29,13 +29,22 @@ public class RapatuCharacterController : CharacterMaster
     public float m_PercentageAdditionalLife = 7;
     RapatuEHealingArea m_HealingAreaE;
     bool m_CanStopE = false;
-    public float m_AmimDelayE = 0.5f;
+    float m_CancelDelayE = 0.5f;
     public float m_ECurrentHealingTime = 0;
     public float m_EMaxHealingTime = 0;
 
+    bool m_SaverCanDoE = true;
+
     [Header("R SKILL")]
+
+    public GameObject m_RIndicatorPrefab;
+    GameObject m_RIndicator;
     public float m_RangeR = 1000;
     public float m_AutoJumpSecondsR = 2;
+    float m_TimerR;
+    bool m_Jumping = false;
+    bool m_SaverCanDoR = true;
+    bool m_RSkillStarted = false;
 
     public override void OnNetworkSpawn()
     {
@@ -50,7 +59,7 @@ public class RapatuCharacterController : CharacterMaster
 
         base.Update();
 
-        if (m_RSkill.GetUsingSkill())
+        if (m_RSkill.GetUsingSkill() && m_RSkillStarted)
         {
             RHoldTimeCheck();
         }
@@ -60,10 +69,22 @@ public class RapatuCharacterController : CharacterMaster
             GetCharacterUI().UpdateCastingUI(m_ECurrentHealingTime, m_EMaxHealingTime);
             if (Input.GetKeyDown(m_ESkillKey))
             {
-                StartCoroutine(StopESkill());
+                StopESkill();
             }
         }
 
+
+        if (GetUseSkillGizmos())
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (m_ESkill.GetUsingSkill())
+                    StartCoroutine(ESkillCoroutine());
+
+                if (m_RSkill.GetUsingSkill() && !m_RSkillStarted)
+                    StartRSkill();
+            }
+        }
     }
 
 
@@ -94,44 +115,52 @@ public class RapatuCharacterController : CharacterMaster
             return;
         if (m_RSkill.GetUsingSkill())
             return;
+        if (!m_SaverCanDoE)
+            return;
+
+
+
+        m_ESkill.SetUsingSkill(true);
+        if (GetUseSkillGizmos())
+        {
+            if (GetShowingGizmos())
+            {
+                m_SkillIndicatorUI.ClearDeletableSkillIndicatorUI();
+                m_SkillIndicatorUI.ClearNormalSkillIndicatorUI();
+                m_SkillIndicatorUI.ClearTargetSkillIndicatorUI();
+            }
+            m_SkillIndicatorUI.CreateCircleSkillIndicator(m_ESkill.m_IndicatorUIObject, m_HealingAreaRadius, transform, false);
+            SetShowingGizmos(true);
+        }
+        else
+            StartCoroutine(ESkillCoroutine());
+
+    }
+
+
+    IEnumerator ESkillCoroutine()
+    {
+        SetShowingGizmos(false);
+        m_ESkill.SetUsingSkill(false);
 
         StopAttacking();
-        base.ESkill();
-        m_ESkill.SetUsingSkill(true);
         SetAnimatorTrigger("IsUsingE");
         SetDisabled(true);
         m_CanStopE = false;
         if (!GetIsLookingForPosition())
             StopMovement();
-        StartCoroutine(ESkillCoroutine());
+        StartCoroutine(ESkillCanStop());
 
         GetCharacterUI().SetCastingUIAbilityText("Curando");
         GetCharacterUI().HideCastingTime();
         GetCharacterUI().UpdateCastingUI(0, 1);
 
-    }
-    IEnumerator StopESkill()
-    {
-        SetAnimatorTrigger("EStop");
-        if (!GetIsLookingForPosition())
-            StopMovement();
-        SetDisabled(false);
-        GetCharacterUI().HideCastingUI();
-        StopCoroutine(ESkillCoroutine());
-        Destroy(m_HealingAreaE.gameObject);
-        yield return new WaitForSeconds(0.1f);
-        m_CanStopE = false;
-        m_ESkill.SetUsingSkill(false);
-    }
 
-    IEnumerator ESkillCoroutine()
-    {
-        yield return new WaitForSeconds(m_AmimDelayE);
-        m_HealingAreaE = Instantiate(m_HealingAreaEPrefab, transform.position, Quaternion.identity).GetComponent<RapatuEHealingArea>();
+
+        m_HealingAreaE = Instantiate(m_HealingAreaEPrefab, transform.position, Quaternion.identity, transform).GetComponent<RapatuEHealingArea>();
         m_HealingAreaE.SetHealingAria(transform, m_HealingAreaRadius / 100);
         m_ECurrentHealingTime = 0;
         m_EMaxHealingTime = m_HealingTimes - 1 * m_HealingTimeSpace;
-        m_CanStopE = true;
         GetCharacterUI().ShowCastingUI();
 
         float l_HealLifeValue = (m_ESkill.GetAttribute("Curación", GetESkillLevel())) + (m_PercentageSkillPower / 100) * GetCharacterStats().GetAbilityPower() + (m_PercentageAdditionalLife / 100) * GetCharacterStats().GetBonusHealth();
@@ -146,8 +175,36 @@ public class RapatuCharacterController : CharacterMaster
         }
 
         if (!m_HealingAreaE.IsDestroyed())
-            StartCoroutine(StopESkill());
+            StopESkill();
     }
+    void StopESkill()
+    {
+        StartCoroutine(RepeatESaver());
+        SetAnimatorTrigger("EStop");
+        if (!GetIsLookingForPosition())
+            StopMovement();
+        SetDisabled(false);
+        GetCharacterUI().HideCastingUI();
+        StopCoroutine(ESkillCoroutine());
+        StopCoroutine(ESkillCanStop());
+        Destroy(m_HealingAreaE.gameObject);
+        base.ESkill();
+        m_CanStopE = false;
+
+    }
+    IEnumerator ESkillCanStop()
+    {
+        yield return new WaitForSeconds(m_CancelDelayE);
+        m_CanStopE = true;
+    }
+    IEnumerator RepeatESaver()
+    {
+        m_SaverCanDoE = false;
+        yield return new WaitForSeconds(0.5f);
+        m_SaverCanDoE = true;
+
+    }
+
     //R SKILL
     protected override void RSkill()
     {
@@ -155,9 +212,34 @@ public class RapatuCharacterController : CharacterMaster
             return;
         if (m_ESkill.GetUsingSkill())
             return;
+        if (!m_SaverCanDoR)
+            return;
 
-        StopAttacking();
         m_RSkill.SetUsingSkill(true);
+
+        if (GetUseSkillGizmos())
+        {
+            m_RSkillStarted = false;
+            if (GetShowingGizmos())
+            {
+                m_SkillIndicatorUI.ClearDeletableSkillIndicatorUI();
+                m_SkillIndicatorUI.ClearNormalSkillIndicatorUI();
+                m_SkillIndicatorUI.ClearTargetSkillIndicatorUI();
+            }
+            m_SkillIndicatorUI.CreateCircleSkillIndicator(m_RSkill.m_IndicatorUIObject, m_RangeR, transform, false);
+            SetShowingGizmos(true);
+        }
+        else
+            StartRSkill();
+
+
+
+    }
+    void StartRSkill()
+    {
+        SetShowingGizmos(false);
+        m_RSkillStarted = true;
+        StopAttacking();
         SetDisabled(true);
         SetAnimatorTrigger("IsUsingR");
         if (!GetIsLookingForPosition())
@@ -167,12 +249,12 @@ public class RapatuCharacterController : CharacterMaster
         GetCharacterUI().HideCastingTime();
         GetCharacterUI().UpdateCastingUI(0, 1);
         GetCharacterUI().ShowCastingUI();
+
+        m_RIndicator = Instantiate(m_RIndicatorPrefab, transform.position, Quaternion.identity, transform);
     }
-    float m_TimerR;
-    public float m_MinTimeToBeRedy = 1f;
-    bool m_Jumping = false;
     void RHoldTimeCheck()
     {
+
         if (!m_Jumping)
         {
             m_TimerR += Time.deltaTime;
@@ -182,15 +264,38 @@ public class RapatuCharacterController : CharacterMaster
                 StartJumpingR();
             }
             GetCharacterUI().UpdateCastingUI(m_TimerR, m_AutoJumpSecondsR);
+
+            if (Input.GetKeyDown(m_RSkillKey) && m_TimerR >= 0.2f)
+            {
+                Debug.LogError("Cancel R");
+                StartCoroutine(RepeatRSaver());
+                GetCharacterUI().HideCastingUI();
+                base.RSkill();
+                m_RSkill.SetTimer(m_RSkill.GetCd() / 2.0f);
+                m_RSkill.SetUsingSkill(false);
+                SetDisabled(false);
+                SetAnimatorTrigger("RJumpStop");
+                Destroy(m_RIndicator);
+            }
+
         }
+
+    }
+    IEnumerator RepeatRSaver()
+    {
+        m_SaverCanDoR = false;
+        yield return new WaitForSeconds(0.5f);
+        m_SaverCanDoR = true;
+
     }
     void StartJumpingR()
     {
-        base.RSkill();
+
         GetCharacterUI().HideCastingUI();
         m_Jumping = true;
         SetAnimatorTrigger("RJump");
-        JumpR();
+        StartCoroutine(JumpR());
+
     }
     public void EndJump()
     {
@@ -199,16 +304,21 @@ public class RapatuCharacterController : CharacterMaster
 
     }
 
-    void JumpR()
+    IEnumerator JumpR()
     {
         Debug.LogError("Jumping");
+        yield return new WaitForSeconds(1f);
+        EndJump();
     }
     void EndRSkill()
     {
+        base.RSkill();
         SetAnimatorTrigger("RJumpStop");
         m_RSkill.SetUsingSkill(false);
         SetDisabled(false);
         m_Jumping = false;
+
+        Destroy(m_RIndicator); // Destruir en el ultimo salto
     }
 
     public override void LevelUpRpc()
